26049c1839725df62f992d1ddf33a1935153b331
benjelloo <55926840+benjelloo@users.noreply.github.com>
2024-01-20 23:15:41 -0800

    ladies and gentlemen we are so back

--- extension/scripts/content.js
+++ extension/scripts/content.js
@@ -25,59 +25,14 @@ fetch(chrome.runtime.getURL('popup/popup.css'))
 console.log('Injected HTML content into the active tab.');
 */
 
-// set up audio stuff
-console.log('hello')
-chrome.runtime.onMessage.addListener(function(message) {
-    console.log("message received");
-    if (message.action == 'startAudioProcessing') {
-        console.log("yepp");
-        state = 1;
-        startAudioProcessing();
-    }
-    if (message.action == 'stopAudioProcessing') {
-        console.log("nopp");
-        state = 0;
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action === 'getElementByClassName') {
+        var elements = document.getElementsByClassName(request.className);
+        console.log(elements);
+        
+        const element = document.querySelector(`[jsname="${request.className}"]`);
+        console.log(element.textContent);
     }
 });
-
-async function startAudioProcessing() {
-    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-    const analyser = audioContext.createAnalyser();
-  
-    // Connect the AnalyserNode to the audioContext
-    analyser.connect(audioContext.destination);
-    
-    console.log("connected!");
-
-    // Capture audio stream from the active tab
-    try {
-      //const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-      const source = audioContext.createMediaStreamSource(stream);
-  
-      // Connect the source to the analyser
-      source.connect(analyser);
-  
-      function processAudioData() {
-        const dataArray = new Uint8Array(analyser.frequencyBinCount);
-        analyser.getByteFrequencyData(dataArray);
-  
-        // Process the audio data and save it into your object
-        const audioObject = {
-          timestamp: Date.now(),
-          audioData: [...dataArray]
-        };
-  
-        // Do something with the audioObject, e.g., send it to a server
-        console.log(audioObject);
-  
-        // Repeat the process
-        requestAnimationFrame(processAudioData);
-      }
-      // Start the audio processing loop
-      processAudioData();
-    } catch (error) {
-      console.error('Error accessing audio:', error);
-    }
-  }
   
 }
